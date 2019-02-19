fce6cd3ecd38d4ce8c79be2d0a0017662bc0938d
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2019-02-19 14:09:05 +0200

    Initial commit

---
+++ app.R
@@ -0,0 +1,186 @@
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(tidyverse)
+
+data <- as.data.frame(readRDS("dataforapp_w_stats.rds"), stringsAsFactors = FALSE)
+
+schools <- unique(data[, "School"])
+
+ui <- function(request) {
+  
+  sidebar <- dashboardSidebar(
+    width = 300,
+    sidebarMenu(
+      selectInput(inputId = "schools",
+                  label = "Top level",
+                  choices = c("All", sort(schools)),
+                  multiple = FALSE,
+                  selected = NULL),
+      selectInput(inputId = "depts",
+                  label = "Department",
+                  choices = NULL,
+                  multiple = FALSE,
+                  selected = NULL),
+      selectInput(inputId = "rgs",
+                  label = "Research group",
+                  choices = NULL,
+                  multiple = FALSE,
+                  selected = NULL)
+    ),
+    tags$div(class="form-group shiny-input-container", 
+             HTML("<p>Altmetrics event data by Crossref as of 2019-02-17</p>
+                  <p><a href='https://blogs.aalto.fi/suoritin/'>About (to do)</a></p>")
+             ))
+  
+  
+  body <- dashboardBody(
+    fluidRow(
+      column(width = 12, 
+             infoBoxOutput("stats_school"),
+             infoBoxOutput("stats_dept"),
+             infoBoxOutput("stats_rg"),
+             valueBoxOutput("toptweet")),
+      column(width = 12,
+             height = "600px",
+             DT::dataTableOutput("summary", 
+                                 width = "100%",
+                                 height = "600px"))
+    )
+  )
+  
+  
+  dashboardPage(
+    dashboardHeader(title = "Tweets on Aalto University publications 2017-", titleWidth = "800"),
+    sidebar,
+    body,
+    skin = "black"
+  )
+  
+}
+
+server <- function(input, output, session) {
+  
+  #-------------------------------------
+  #
+  # Top level
+  # 
+  # Schools, Centres of Excellence,
+  # University level
+  #
+  #-------------------------------------
+  
+ schoolData <- reactive({
+    if ( input$schools == "All" ) return(data) else data[data$School %in% input$schools, ]
+  })
+
+  #--------------------
+  #
+  # Department level
+  #
+  #--------------------
+  
+  observe (
+    updateSelectInput(session, 
+                      inputId = 'depts', 
+                      choices = if ( input$schools == "All" ) "Select top level" else c("All", sort(unique(schoolData()$`Department or research area`)))
+    )
+  )
+  
+  deptData <- reactive({
+    if ( is.null(input$depts) || input$depts == "Select top level" || input$depts == "All") {
+      return(schoolData())
+    }
+    isolate(schoolData() %>% filter( `Department or research area` %in% input$depts ))
+    
+  })
+  
+  #----------------------
+  #
+  # Research group level
+  #
+  #----------------------
+  
+  observe(
+    updateSelectInput(session,
+                      inputId = 'rgs',
+                      choices = if ( input$depts == "Select top level" || input$depts == "All") "Select department" else c("All", sort(unique(deptData()$`Research group`)))
+    )
+  )
+
+  rgData <- reactive({
+    if ( is.null(input$rgs) || input$rgs == "Select department" || input$rgs == "All") {
+      return(deptData())
+    }
+    isolate(deptData() %>%  filter(`Research group` %in% input$rgs))
+
+  })
+  
+ 
+  output$stats_school <- renderInfoBox({
+    infoBox(
+      "Tweets at top level",
+     paste0(rgData()$Tweets_by_school[1], " (",  rgData()$Tweets_article_ratio_school[1], ")"),
+     icon = icon("twitter"),
+     width = NULL
+    )
+  })
+  
+  output$stats_dept <- renderInfoBox({
+    infoBox(
+      "Tweets at department level",
+      paste0(rgData()$Tweets_by_dept[1], " (", rgData()$Tweets_article_ratio_dept[1], ")"),
+      icon = icon("twitter"),
+      color = "olive",
+      width = NULL
+    )
+  })
+  
+  output$stats_rg <- renderInfoBox({
+    infoBox(
+      "Tweets at research group level",
+      paste0(rgData()$Tweets_by_rg[1], " (",  rgData()$Tweets_article_ratio_rg[1], ")"),
+      icon = icon("twitter"),
+      color = "orange",
+      width = NULL
+    )
+  })
+  
+
+  output$toptweet <- renderValueBox({
+    valueBox(
+      value = "Top tweeted",
+      ifelse(max(rgData()$Tweets_by_article) == 0, "-",
+             paste0(rgData()[rgData()$Tweets_by_article == max(rgData()$Tweets_by_article), "title"][1], " (" , rgData()[rgData()$Tweets_by_article == max(rgData()$Tweets_by_article), "Tweets_by_article"][1], " times)")),
+      icon = icon("twitter"),
+      color = "orange",
+      width = 12,
+      href = ifelse(max(rgData()$Tweets_by_article) == 0, "-", 
+                    str_extract(rgData()[rgData()$Tweets_by_article == max(rgData()$Tweets_by_article), "Article"][1], "https://[^']+"))
+    )
+  })
+  
+  
+  totable <- reactive({
+    df <- rgData()
+    
+    df <- df %>% 
+      rename(Tweets = Tweets_by_article) %>% 
+      select(School, `Department or research area`, `Research group`, Year, Article, Tweet, Date)
+  })
+
+  
+  output$summary <- DT::renderDataTable({
+    dat <- datatable(totable(), 
+                     escape = FALSE, 
+                     rownames = FALSE,
+                     filter = "top",
+                     options(list(pageLength = 50)))
+    return(dat)
+    
+  })
+  
+}
+
+
+shinyApp(ui = ui, server = server)
---
+++ parse.R
@@ -0,0 +1,105 @@
+library(crevents)
+library(tidyverse)
+library(readxl)
+
+aalto <- read.csv("DOI_q-08_02_2019.csv", stringsAsFactors = FALSE)
+names(aalto) <- c("unit", "parent", "doi", "title", "subtitle", "journal", "year", "isbn", "issn", "isbne")
+
+withdois <- aalto %>% 
+  filter(doi != '') %>% 
+  distinct(doi, .keep_all = TRUE) %>% 
+  select(-isbn, -isbne)
+
+dois <- as.list(withdois[, "doi"])
+
+tweeted <- function(doi, from, to){
+  print(paste0("Querying ", doi))
+  x <- crev_query(obj_id = doi, source = "twitter", # with default row limit 1000
+                  from_occurred_date = from, until_occurred_date = to)
+  return(x$message$events)
+}
+
+res <- map_dfr(dois, ~ tweeted(.x, "2017-01-01", "2019-02-17"))
+
+write.csv(res, "events.csv", row.names = FALSE)
+
+res <- res %>% 
+  mutate(doi = substr(obj_id, 17, length(obj_id))) 
+
+alldata <- left_join(withdois, res)
+
+write.csv(alldata, "alldata.csv", row.names = FALSE)
+
+data2app <- alldata %>% 
+  rename(tweet = subj.pid) %>% 
+  filter(unit != 'Not published at Aalto University') %>% 
+  mutate(year = str_sub(year, -4)) %>% 
+  select(unit, parent, doi, obj_id, title, year, occurred_at, tweet)
+
+#-------------------------
+#
+# Join with org data
+#
+#------------------------
+
+org <- read_excel("org_hierarchy.xls", sheet = "Sheet2")
+
+# The managing unit in the Pure record can be from any level (rg, dept, school, univ)
+data_org_rg <- inner_join(org, data2app, by = c("Research group"="unit"))
+data_org_dept <- inner_join(org, data2app, by = c("Department or research area"="unit"))
+data_org_school <- inner_join(org, data2app, by = c("School"="unit"))
+data_org_univ <- inner_join(org, data2app, by = c("University or research org"="unit"))
+
+data_org_raw <- rbind(data_org_rg, data_org_dept, data_org_school, data_org_univ)
+
+data_org <- data_org_raw %>% 
+  distinct(doi, title, occurred_at, .keep_all = TRUE)
+
+dataforapp <- data_org %>% 
+  rename(Date = occurred_at,
+         Tweet = tweet,
+         Year = year) %>% 
+  mutate(Tweet = ifelse(!is.na(Tweet), paste0("<a target='blank' href='", Tweet, "'>Tweet</a>"), ""),
+         Article = paste0("<a target='blank' href='https://doi.org/", doi, "'>", title, "</a>")) %>% 
+  select(School, `Department or research area`, `Research group`, Year, Article, title, Tweet, Date) %>% 
+  arrange(School, `Department or research area`, `Research group`, Year, Article, Date)
+
+
+#------------------------
+# 
+# Twitter stats 
+#
+#------------------------
+
+stats_raw <- dataforapp %>% 
+  group_by(School) %>% 
+  mutate(Articles_by_school = n(),
+         Tweets_by_school = sum(Tweet != ""),
+         Tweets_article_ratio_school = paste0(round(Tweets_by_school/Articles_by_school,1),"%")) %>% 
+  arrange(desc(Tweets_by_school)) %>% 
+  ungroup() %>% 
+  group_by(School, `Department or research area`) %>% 
+  mutate(Articles_by_dept = n(),
+         Tweets_by_dept = sum(Tweet != ""),
+         Tweets_article_ratio_dept = paste0(round(Tweets_by_dept/Articles_by_dept,1),"%")) %>% 
+  arrange(desc(Tweets_by_dept)) %>% 
+  ungroup() %>% 
+  group_by(School, `Department or research area`,`Research group`) %>% 
+  mutate(Articles_by_rg = n(),
+         Tweets_by_rg = sum(Tweet != ""),
+         Tweets_article_ratio_rg = paste0(round(Tweets_by_rg/Articles_by_rg,1),"%")) %>% 
+  arrange(desc(Tweets_by_rg)) %>% 
+  ungroup() %>% 
+  group_by(School, `Department or research area`, `Research group`, Article) %>% 
+  mutate(Tweets_by_article = sum(Tweet != "")) %>% 
+  ungroup() 
+
+dataforapp_w_stats <- left_join(dataforapp, stats_raw)
+
+saveRDS(dataforapp_w_stats, "dataforapp_w_stats.rds")
+
+
+
+
+
+
